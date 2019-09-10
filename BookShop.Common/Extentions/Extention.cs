a825f5aed4edb798689986bb8c4a4653f5f29cab
donstany <donstany79@gmail.com>
2019-09-10 16:32:44 +0300

    Initial commit

---
+++ BookShop.Common/Extentions/Extention.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Common.Extentions
+{
+    public static class Extention
+    {
+        private const string PricePropertyName = "price";
+        
+        public static string JoinWithNewLine<T>(this IEnumerable<T> collection)
+        {
+            return string.Join($"{Environment.NewLine}", collection);
+        }
+
+        public static string BuildStringFromDTO<T>(this IEnumerable<T> collection,
+                                                                    string separator = " ",
+                                                                    string currencySymbol = "",
+                                                                    string pricePropertyName = PricePropertyName)
+        {
+            var sb = new StringBuilder();
+            var properties = typeof(T).GetProperties();
+
+            foreach (var item in collection)
+            {
+                foreach (var property in properties)
+                {
+                    if (property.Name.Trim().ToLower() == pricePropertyName)
+                    {
+                        sb.Append(currencySymbol + property.GetValue(item));
+                    }
+                    else
+                    {
+                        sb.Append(property.GetValue(item));
+                    }
+                    sb.Append(separator);
+                }
+                int separatorLength = separator.Length;
+                sb.Remove(sb.Length - separatorLength, separatorLength);
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ BookShop/StartUp.cs
@@ -0,0 +1,76 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using BookShop.Common.Extentions;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                DbInitializer.ResetDatabase(db);
+
+                var getBooksByAgeRestriction = GetBooksByAgeRestriction(db, "miNor");
+                //Console.WriteLine(getBooksByAgeRestriction);
+                //Console.WriteLine(GetGoldenBooks(db));
+                //Console.WriteLine(GetBooksByPrice(db));
+                Console.WriteLine(GetBooksNotReleasedIn(db, 2000));
+                Console.ReadKey();
+            }
+        }
+
+        //1.Age Restriction
+        //Return in a single string all book titles, each on a new line, that have age restriction, equal to the given command. Order the titles alphabetically.
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var result = context.Books.Where(x => x.AgeRestriction.ToString().ToLower() == command.ToLower())
+                                      .Select(x => x.Title)
+                                      .OrderBy(x => x).ToList();
+
+            return result.JoinWithNewLine();
+        }
+
+        //2.Golden Books
+        //Return in a single string titles of the golden edition books that have less than 5000 copies, each on a new line. Order them by book id ascending
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var result = context.Books.Where(book => book.Copies < 5000)
+                                        //.Select(book => new { book.Title, book.BookId })
+                                        .OrderBy(goldenBook => goldenBook.BookId)
+                                         //.Select(goldenBook => new { goldenBook.Title, goldenBook.BookId }).ToList();
+                                         .Select(goldenBook => goldenBook.Title).ToList();
+            return result.JoinWithNewLine();
+        }
+
+        //3.Books by Price
+        //Return in a single string all titles and prices of books with price higher than 40, each on a new row in the format given below.Order them by price descending
+        //O Pioneers! - $49.90
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var result = context.Books.Where(book => book.Price > 40)
+                                        //.Select(x => new { x.Title, x.Price })
+                                        .OrderByDescending(x => x.Price)
+                                        .Select(x => new { x.Title, x.Price }).ToList();
+
+            return result.BuildStringFromDTO(currencySymbol:"$", separator:" - ");
+        }
+
+        //4. Not Released In
+        //Return in a single string all titles of books that are NOT released on a given year.Order them by book id ascending.
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+
+            var result = context.Books.Where(x => x.ReleaseDate.GetValueOrDefault().Year != year)
+                                        .OrderBy(x => x.BookId)
+                                        .Select(x => x.Title);
+
+            return result.JoinWithNewLine();
+        }
+    }
+}
